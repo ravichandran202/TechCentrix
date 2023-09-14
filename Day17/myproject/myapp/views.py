b044a312541afd785e3ac6021df21265b467fe35
Ravichandran T S <ravichandrants202@gmail.com>
2023-09-14 12:28:49 +0530

    Updated with Contact and about

--- Day17/myproject/myapp/views.py
+++ Day17/myproject/myapp/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> WELCOME </h1>')
+    return HttpResponse(f'<h1> WELCOME </h1>')
+
+def about(request):
+    return HttpResponse(f'<h1> ABOUT </h1>')
+
+def contact(request):
+    return HttpResponse(f'<h1> CONTACT </h1>')
--- Day17/myproject/myproject/urls.py
+++ Day17/myproject/myproject/urls.py
@@ -20,5 +20,7 @@ from myapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-     path("", views.home, name='home'),
+    path("", views.home, name='home'),
+    path("about", views.about, name='about'),
+    path("contact", views.contact, name='contact'),
 ]
