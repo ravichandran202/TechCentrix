0b197095e98e16e18946733a089b4348dfafbbdc
Ravichandran T S <ravichandrants202@gmail.com>
2023-09-15 15:08:13 +0530

    created templates

--- Day18/myproject/myapp/views.py
+++ Day18/myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
