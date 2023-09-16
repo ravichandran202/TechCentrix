8c6943302385c89743d188b729109e0d8b523758
Ravichandran T S <ravichandrants202@gmail.com>
2023-09-16 15:22:20 +0530

    creating Contact Table in Database

--- Day19/myproject/myproject/settings.py
+++ Day19/myproject/myproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": 'techcentrix',
         "USER" : 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST':'localhost',
         'PORT':'3306'
     }
