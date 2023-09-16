41e969fa92b5c1bcf5ce8f0fdc52703fb4c00a24
Ravichandran T S <ravichandrants202@gmail.com>
2023-09-16 15:28:14 +0530

    Added some functionalities to store the submitted data from contact-us webpage

--- Day19/myproject/myapp/views.py
+++ Day19/myproject/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -8,6 +8,11 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        Contact(name=name,email=email).save()
+        
     return render(request,'contact.html')
 
 def gallery(request):
